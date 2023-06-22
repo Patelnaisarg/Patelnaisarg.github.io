2fbb52427ed94e882c23a583ef6bd9936fdd5c32
Patelnaisarg <110363151+Patelnaisarg@users.noreply.github.com>
2023-06-23 11:27:39 +1200

    Add files via upload

---
+++ currentbooking.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Current Booking</title>
+</head>
+<body>
+<table border="1">
+  <tr>
+    <th>Booking(Room,Date)</th>
+    <th>customer</th>
+    <th>Actoin</th>
+  </tr>
+  <tr>
+    <td>Kallie,Date(check in),(checkout)</td>
+    <td>rock,Billu</td>
+    <td>[view][edit][manage review]</td>
+  </tr>
+  <tr>
+  <td>Kallie,Date(check in),(checkout)</td>
+    <td>Roman,reigns</td>
+    <td>[view][edit][manage review]</td>
+  </tr>
+  <tr>
+  <td>Kallie,Date(check in),(checkout)</td>
+    <td>Gold,berg</td>
+    <td>[view][edit][manage review]</td>
+  </tr>
+  
+ 
+</table>
+    
+</body>
+</html>
